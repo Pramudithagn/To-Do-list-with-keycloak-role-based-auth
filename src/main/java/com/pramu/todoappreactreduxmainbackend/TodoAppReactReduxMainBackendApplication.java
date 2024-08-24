81b5e1ccf3cacb4213132702118c3e40974c430b
Pramuditha Gunaratne <pramudithagn@gmail.com>
2024-08-24 18:45:47 +0530

    1

---
+++ src/main/java/com/pramu/todoappreactreduxmainbackend/TodoAppReactReduxMainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pramu.todoappreactreduxmainbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppReactReduxMainBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppReactReduxMainBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pramu/todoappreactreduxmainbackend/TodoAppReactReduxMainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramu.todoappreactreduxmainbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppReactReduxMainBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
